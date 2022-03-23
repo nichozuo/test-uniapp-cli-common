9b79048b88a0f8786d477d1f2532bfc4ac001224
zuowb <zuowbhk@gmail.com>
2022-03-23 17:39:39 +0800

    add tailwind

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue";
+import App from "./App";
+import "tailwindcss/tailwind.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-App.mpType = 'app'
+App.mpType = "app";
 
 const app = new Vue({
-  ...App
-})
-app.$mount()
+  ...App,
+});
+app.$mount();
